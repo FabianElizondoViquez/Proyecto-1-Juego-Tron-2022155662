282c678fccab202fda2590bf8c158496b71e02f8
Fabián <fabilu242003@gmail.com>
2024-09-02 17:23:10 -0600

    Prueba

--- obj/Debug/net8.0-windows/Proyecto-1-Juego-Tron-2022155662.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Proyecto-1-Juego-Tron-2022155662.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto-1-Juego-Tron-2022155662")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1b863dd0a7a7d4dffdda88113ee043c0d1d352")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28fd65ef1a29dded99fff81b66f0ea9ea77faaf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto-1-Juego-Tron-2022155662")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto-1-Juego-Tron-2022155662")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
